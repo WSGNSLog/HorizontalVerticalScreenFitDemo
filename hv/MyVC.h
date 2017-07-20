a8d355d675b0171280f53929cd84e8c410cd10ad
wushiguang <wushiguang@jiuan.com>
2017-07-20 16:16:59 +0800

    横竖屏xib适配测试demo
    横竖屏xib适配测试demo

---
+++ hv/MyVC.h
@@ -0,0 +1,13 @@
+//
+//  MyVC.h
+//  hv
+//
+//  Created by wuxue on 2017/6/1.
+//  Copyright © 2017年 wsg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyVC : UIViewController
+
+@end
---
+++ hv/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  hv
+//
+//  Created by wuxue on 2017/5/31.
+//  Copyright © 2017年 wsg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
